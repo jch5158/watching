8acb3e4f36edbba277c6abf2c6fd4a4afee89fb9
Chanhun Jeong <jch3578@icloud.com>
2022-12-05 13:08:48 +0900

    feat : 환경 변수 로드 기능 추가
    - dotenv 패키지를 추가하여 .env 파일에서 환경 변수 로드 기능 추가

--- src/server/entry/server.js
+++ src/server/entry/server.js
@@ -1,8 +1,7 @@
+import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import rootRouter from "../routers/rootRouter";
-
-const PORT = 7000;
 
 const app = express();
 const logger = morgan("dev");
@@ -10,6 +9,6 @@ const logger = morgan("dev");
 app.use(logger);
 app.use("/", rootRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server listening PORT : ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening PORT : ${process.env.PORT}`);
 });
