72a3839e70a684d5710dc049a6ffd8e1c5661075
Chanhun Jeong <jch3578@icloud.com>
2022-12-07 11:36:30 +0900

    feat : 메일 송신 기능 추가
    - 이메일 인증 기능을 위해 nodemailer 패키지 추가 후 메일 송신 기능 추가
    - 메일 템플릿 작성을 위한 nodemailer-pug-engine 패키지 추가
    - pug 메일 템플릿 추가

--- src/server/controllers/usersController.js
+++ src/server/controllers/usersController.js
@@ -1,12 +1,52 @@
 import User from "../models/User";
+import nodemailer from "nodemailer";
+import { pugEngine } from "nodemailer-pug-engine";
 
-function validateName(name) {
+const transporter = nodemailer.createTransport({
+  serviec: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_SERVER_ID,
+    pass: process.env.SMTP_SERVER_PASS,
+  },
+});
+
+transporter.use(
+  "compile",
+  pugEngine({
+    templateDir: `${process.cwd()}/src/client/views/screens/mails`,
+  })
+);
+
+const data = {
+  from: process.env.SMTP_SERVER_ID,
+  to: process.env.SMTP_SERVER_ID,
+  subject: "subject",
+  template: "mail-authentication",
+  ctx: {
+    code: "good~",
+  },
+};
+
+const send = async (data) => {
+  await transporter.sendMail(data, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      return info.response;
+    }
+  });
+};
+
+const validateName = (name) => {
   // 한글 2자 이상 15자 이하 허용
   const regExp = /^[가-힣]{2,20}$/;
   return regExp.test(name);
-}
+};
 
-function validateEmail(email) {
+const validateEmail = (email) => {
   if (email.length < 5 || email.length > 320) {
     return false;
   }
@@ -15,16 +55,16 @@ function validateEmail(email) {
     /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
 
   return regExp.test(email);
-}
+};
 
-function validateNickName(nickname) {
+const validateNickname = (nickname) => {
   //- 2자 이상 15자 이하, 영어 또는 숫자 또는 한글로 구성
   //* 특이사항 : 한글 초성 및 모음은 허가하지 않는다.
   const regExp = /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,15}$/;
   return regExp.test(nickname);
-}
+};
 
-function validatePassword(password, passwordConfirm) {
+const validatePassword = (password, passwordConfirm) => {
   if (password !== passwordConfirm) {
     return false;
   }
@@ -34,9 +74,9 @@ function validatePassword(password, passwordConfirm) {
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,15}$/;
 
   return regExp.test(password);
-}
+};
 
-function validateFile(file) {
+const validateFile = (file) => {
   if (
     file.mimetype !== "image/jpg" ||
     file.mimetype !== "image/jpeg" ||
@@ -46,7 +86,7 @@ function validateFile(file) {
   }
 
   return true;
-}
+};
 
 export const getJoin = (req, res) => {
   res.render("screens/users/join", { pageTitle: "회원가입" });
@@ -63,7 +103,7 @@ export const postJoin = async (req, res) => {
   if (
     !validateName(name) ||
     !validateEmail(email) ||
-    !validateNickName(nickname) ||
+    !validateNickname(nickname) ||
     !validatePassword(password, password_confirm) ||
     !file
   ) {
