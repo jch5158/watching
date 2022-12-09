b1aebec61ce7b68b9addc18ad3aafd180f5fd4c8
Chanhun Jeong <jch3578@icloud.com>
2022-12-09 17:27:06 +0900

    refactor : 유저 정보 검증 코드 파일 분리
    - 코드 가독성을 위해서 유저 정보를 검증하는 파일을 별도로 분리

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -4,6 +4,15 @@ import { pugEngine } from "nodemailer-pug-engine";
 import { webcrypto } from "crypto";
 import redisClient from "../entry/initRedis";
 import bcrypt from "bcrypt";
+import {
+  validateName,
+  validateEmail,
+  validateNickname,
+  validatePassword,
+  validateImageFile,
+  validateAuthenticode,
+  validateToken,
+} from "../validators/users/userValidator";
 
 const transporter = nodemailer.createTransport({
   serviec: "gmail",
@@ -41,69 +50,6 @@ const sendEmail = async (data) => {
       return info.response;
     }
   });
-};
-
-const validateName = (name) => {
-  // 한글 2자 이상 15자 이하 허용
-  const regExp = /^[가-힣]{2,20}$/;
-  return regExp.test(name);
-};
-
-const validateEmail = (email) => {
-  if (!email || email.length < 5 || email.length > 320) {
-    return false;
-  }
-
-  const regExp =
-    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
-
-  return regExp.test(email);
-};
-
-const validateNickname = (nickname) => {
-  //- 2자 이상 15자 이하, 영어 또는 숫자 또는 한글로 구성
-  //* 특이사항 : 한글 초성 및 모음은 허가하지 않는다.
-  const regExp = /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,15}$/;
-  return regExp.test(nickname);
-};
-
-const validatePassword = (password) => {
-  //최소 8자 + 최대 15자 + 최소 한개의 소문자 + 최소 한개의 대문자 + 최소 한개의 숫자 + 최소 한개의 특수 문자
-  const regExp =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,15}$/;
-
-  return regExp.test(password);
-};
-
-const validateImageFile = (file) => {
-  if (!file) {
-    return false;
-  }
-
-  if (
-    file.mimetype !== "image/jpg" &&
-    file.mimetype !== "image/jpeg" &&
-    file.mimetype !== "image/png"
-  ) {
-    return false;
-  }
-
-  return true;
-};
-
-const validateAuthenticode = (authenticode) => {
-  if (!authenticode || authenticode < 6) {
-    return false;
-  }
-  return true;
-};
-
-const validateToken = (token) => {
-  if (!token || token.length !== 10) {
-    return false;
-  }
-
-  return true;
 };
 
 export const getJoin = (req, res) => {
---
+++ src/server/validators/users/userValidator.js
@@ -0,0 +1,62 @@
+export const validateName = (name) => {
+  // 한글 2자 이상 15자 이하 허용
+  const regExp = /^[가-힣]{2,20}$/;
+  return regExp.test(name);
+};
+
+export const validateEmail = (email) => {
+  if (!email || email.length < 5 || email.length > 320) {
+    return false;
+  }
+
+  const regExp =
+    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+  return regExp.test(email);
+};
+
+export const validateNickname = (nickname) => {
+  //- 2자 이상 15자 이하, 영어 또는 숫자 또는 한글로 구성
+  //* 특이사항 : 한글 초성 및 모음은 허가하지 않는다.
+  const regExp = /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,15}$/;
+  return regExp.test(nickname);
+};
+
+export const validatePassword = (password) => {
+  //최소 8자 + 최대 15자 + 최소 한개의 소문자 + 최소 한개의 대문자 + 최소 한개의 숫자 + 최소 한개의 특수 문자
+  const regExp =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,15}$/;
+
+  return regExp.test(password);
+};
+
+export const validateImageFile = (file) => {
+  if (!file) {
+    return false;
+  }
+
+  if (
+    file.mimetype !== "image/jpg" &&
+    file.mimetype !== "image/jpeg" &&
+    file.mimetype !== "image/png"
+  ) {
+    return false;
+  }
+
+  return true;
+};
+
+export const validateAuthenticode = (authenticode) => {
+  if (!authenticode || authenticode < 6) {
+    return false;
+  }
+  return true;
+};
+
+export const validateToken = (token) => {
+  if (!token || token.length !== 10) {
+    return false;
+  }
+
+  return true;
+};
