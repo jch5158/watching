e247f0a85b61ceaa5969fc5e11039eb7fd6fe01f
Chanhun Jeong <jch3578@icloud.com>
2022-12-10 12:47:45 +0900

    feat : 이메일 보안 사용
    - TLS를 이용한 이메일 보안 사

--- src/server/modules/mailer.js
+++ src/server/modules/mailer.js
@@ -5,6 +5,8 @@ const transporter = nodemailer.createTransport({
   serviec: process.env.SMTP_SERVICE_NAME,
   host: process.env.SMTP_SERVER_HOST,
   port: process.env.SMTP_SERVER_PORT,
+  // TLS를 이용해서 이메일 통신 암호화
+  secure: true,
   auth: {
     user: process.env.SMTP_SERVER_ID,
     pass: process.env.SMTP_SERVER_PASS,
