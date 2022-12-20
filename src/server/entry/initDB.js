7ccb5a82535848e6f99316de43ed3f0e390c9d96
Chanhun Jeong <jch3578@icloud.com>
2022-12-20 21:55:40 +0900

    .

--- src/server/entry/initDB.js
+++ src/server/entry/initDB.js
@@ -4,6 +4,4 @@ const db = mongoose.connection;
 db.on("error", (error) => console.log(`DB Error\n${error}`));
 db.once("open", () => console.log("Connected to DB"));
 
-(async () => {
-  await mongoose.connect(process.env.DB_URL);
-})();
+mongoose.connect(process.env.DB_URL);
